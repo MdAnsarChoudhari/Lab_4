89228486de8f1e8a3ffec233588bd9cac9116b27
Md Ansar choudhari <muhammadansarchoudhari@gmail.com>
2025-10-02 12:53:47 +0530

    Create add_sub.py

---
+++ add_sub.py
@@ -0,0 +1,7 @@
+a=int(input("Enter first Number : "))
+b=int(input("Enter Second Number : "))
+sum=a+b
+sub=a-b
+
+print(f"Addition of {a} and {b} is {sum} ")
+print(f"Substraction of {a} and {b} is {sub} ")
