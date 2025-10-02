e2b4b1ac14be5284a20cd3ad04691edbab934cb1
Muhammad Ansar M Choudhari <muhammadansarchoudhari@gmail.com>
2025-10-02 12:59:59 +0530

    updated

--- add_sub.py
+++ add_sub.py
@@ -5,3 +5,9 @@ sub=a-b
 
 print(f"Addition of {a} and {b} is {sum} ")
 print(f"Substraction of {a} and {b} is {sub} ")
+
+multiplication=a*b
+div=a/b
+
+print(f"Multiplication of {a} and {b} is {multiplication} ")
+print(f"Division of {a} and {b} is {div} ")
